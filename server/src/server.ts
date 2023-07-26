130096a5220c2f249268967ba48de3745b9a2fe2
Ariane Brandão <ariane@ghbranding.com.br>
2023-07-26 18:12:53 -0300

    feat: adiciona rota de upload de arquivos no backend

--- server/src/server.ts
+++ server/src/server.ts
@@ -3,10 +3,21 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import jwt from '@fastify/jwt'
 import cors from '@fastify/cors'
+import multipart from '@fastify/multipart'
 import { memoriesRoutes } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
+
+app.register(multipart)
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+app.register(require('@fastify/static'), {
+  root: resolve(__dirname, '../uploads'),
+  prefix: '/uploads',
+})
 
 app.register(cors, {
   origin: true, // todas URLS de front-end poderão acessar esse back-end
@@ -18,6 +29,7 @@ app.register(jwt, {
 })
 
 app.register(authRoutes)
+app.register(uploadRoutes)
 app.register(memoriesRoutes)
 
 app
