c66329aa61e58e940730f569e869ea19ecde1da2
Ariane Brandão <ariane@ghbranding.com.br>
2023-07-26 17:34:29 -0300

    feat: adiciona logout e página para criar memória

--- frontend/src/components/Copyright.tsx
+++ frontend/src/components/Copyright.tsx
@@ -1,24 +1,26 @@
+import Link from 'next/link'
+
 export function Copyright() {
   return (
     <div className="text-sm leading-relaxed text-gray-200">
       Feito com 💜 por{' '}
-      <a
+      <Link
         href="https://arianebrandao.github.io"
         target="_blank"
         rel="noreferrer"
         className="underline hover:text-gray-100"
       >
         Ariane Brandão
-      </a>{' '}
+      </Link>{' '}
       no NLW da{' '}
-      <a
+      <Link
         target="_blank"
         rel="noreferrer"
         className="underline hover:text-gray-100"
         href="https://rocketseat.com.br"
       >
         Rocketseat
-      </a>
+      </Link>
     </div>
   )
 }
